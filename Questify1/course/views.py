df1961550540ab3cc18869ac496a5b8ded191319
KozinetsJane <kozinets15@gmail.com>
2025-08-21 11:24:31 +0200

    new url

--- Questify1/course/views.py
+++ Questify1/course/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def course_list(request):
+    return HttpResponse("Здесь будет выведен список курсов.")
 
 # Create your views here.
