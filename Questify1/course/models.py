85385b92e92244767e64940e65dd666fa4c86256
KozinetsJane <kozinets15@gmail.com>
2025-08-21 16:38:53 +0200

    minor fixes

--- Questify1/course/models.py
+++ Questify1/course/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    
 # Create your models here.
