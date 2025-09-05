9dfac5dc584cd33fec2719b745af887cbbb25bdd
KozinetsJane <kozinets15@gmail.com>
2025-09-05 12:44:30 +0200

    features choise, teacher, category

--- Questify1/course/admin.py
+++ Questify1/course/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Teacher, Category
 
 admin.site.register(Course)
-
+admin.site.register(Teacher)
+admin.site.register(Category)
 
 # Register your models here.
--- Questify1/course/models.py
+++ Questify1/course/models.py
@@ -2,11 +2,45 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+    name = models.CharField("Название категории", max_length=30, unique=True)
+
+    def __str__(self):
+            return f"{self.name}"
+    
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        
+
+class Teacher(models.Model):
+    name = models.CharField("Имя", max_length=30)
+    surname = models.CharField("Фамилия", max_length=50)
+    patronymic = models.CharField("Отчество", max_length=50, null=True, blank=True)
+    years_of_experience = models.PositiveIntegerField("Опыт преподавания (лет)")
+
+    class Meta:
+        verbose_name = "Преподаватель"
+        verbose_name_plural = "Преподаватели"
+        ordering = ["surname", "name"]
+
+    def __str__(self):
+        return f"{self.surname}, {self.name}, {self.patronymic}"
+
+
 class Course(models.Model):
+    class LevelChoices(models.IntegerChoices): 
+        BEGINNER = 1, "Начальный" 
+        INTERMEDIATE = 2, "Средний" 
+        ADVANCED = 3, "Продвинутый"
+
     title = models.CharField(max_length=200, verbose_name="Название", help_text="Название курса")
     description = models.TextField(null=True, verbose_name="Описание", blank=True)
     price = models.FloatField(null=True, verbose_name="Цена", blank=True)
     published = models.DateTimeField(db_index=True, verbose_name="Опубликовано", default=timezone.now)
+    level = models.SmallIntegerField("Уровень сложности", choices=LevelChoices.choices, default=LevelChoices.BEGINNER)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, blank=True, null=True, related_name="course")
+    category = models.ManyToManyField(Category, "Категория", null=True)
 
     def __str__(self):
         return f"{self.title} {self.price}"
